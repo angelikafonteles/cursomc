4a6470c09deacebf55f805e4a3be65423e29e9a0
Angelika Fonteles <angelikatf@gmail.com>
2020-10-26 16:06:33 -0300

    adicionando endpoints

--- src/main/java/com/angelikafonteles/cursomc/domain/Cliente.java
+++ src/main/java/com/angelikafonteles/cursomc/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.angelikafonteles.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Cliente {
@@ -27,6 +28,7 @@ public class Cliente {
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
--- src/main/java/com/angelikafonteles/cursomc/domain/Endereco.java
+++ src/main/java/com/angelikafonteles/cursomc/domain/Endereco.java
@@ -7,6 +7,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Endereco {
 	private static final long serialVersionUID = 1L;
@@ -24,6 +26,7 @@ public class Endereco {
 	@JoinColumn(name="cidade_id")
 	private Cidade cidade;
 	
+	@JsonBackReference
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
