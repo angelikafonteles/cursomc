1f0ea6da0cb8af395b8b2e16aae1b61329353378
Angelika Fonteles <angelikatf@gmail.com>
2020-12-10 11:53:55 -0300

    Inserindo uma metodo POST

--- src/main/java/com/angelikafonteles/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/angelikafonteles/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,16 @@
 package com.angelikafonteles.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.angelikafonteles.cursomc.domain.Categoria;
 import com.angelikafonteles.cursomc.services.CategoriaService;
@@ -23,4 +28,10 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@PostMapping
+	public ResponseEntity<Categoria> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
