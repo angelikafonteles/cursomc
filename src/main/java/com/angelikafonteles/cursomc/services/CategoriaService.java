8a090895e706681931df2c25141f7ef81e330aa7
Angelika Fonteles <angelikatf@gmail.com>
2020-12-10 14:05:30 -0300

    listando todas as categorias com DTO

--- src/main/java/com/angelikafonteles/cursomc/services/CategoriaService.java
+++ src/main/java/com/angelikafonteles/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.angelikafonteles.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,34 +14,37 @@ import com.angelikafonteles.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
+
 	public Categoria find(Integer id) {
-		
+
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-	"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
 			repo.deleteById(id);
-		}catch(DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
 		}
 	}
 }
-
