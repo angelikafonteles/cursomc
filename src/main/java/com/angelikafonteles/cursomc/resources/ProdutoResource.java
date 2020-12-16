22f1e3bc5ce11959a70a1c2c34e45d673d99519d
Angelika Fonteles <angelikatf@gmail.com>
2020-12-16 17:02:40 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/angelikafonteles/cursomc/resources/ProdutoResource.java
+++ src/main/java/com/angelikafonteles/cursomc/resources/ProdutoResource.java
@@ -1,13 +1,19 @@
 package com.angelikafonteles.cursomc.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.angelikafonteles.cursomc.domain.Produto;
+import com.angelikafonteles.cursomc.dto.ProdutoDTO;
+import com.angelikafonteles.cursomc.resources.utils.URL;
 import com.angelikafonteles.cursomc.services.ProdutoService;
 
 @RestController
@@ -21,6 +27,21 @@ public class ProdutoResource {
 	public ResponseEntity<Produto> find(@PathVariable Integer id) {
 		Produto obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<ProdutoDTO>> findPage(
+			@RequestParam(value="nome", defaultValue = "") String nome,
+			@RequestParam(value="categorias", defaultValue = "") String categorias,
+			@RequestParam(value="page", defaultValue = "0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue = "24")Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue = "nome")String orderBy,
+			@RequestParam(value="direction", defaultValue = "ASC")String direction) {
+		String nomeDecoded = URL.decodeParam(nome);
+		List<Integer> ids = URL.decodeIntList(categorias);
+		Page<Produto> list = service.search(nomeDecoded, ids, page, linesPerPage, orderBy, direction);
+		Page<ProdutoDTO> listDto = list.map(obj -> new ProdutoDTO(obj));
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
--- src/main/java/com/angelikafonteles/cursomc/services/ProdutoService.java
+++ src/main/java/com/angelikafonteles/cursomc/services/ProdutoService.java
@@ -1,11 +1,17 @@
 package com.angelikafonteles.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.angelikafonteles.cursomc.domain.Categoria;
 import com.angelikafonteles.cursomc.domain.Produto;
+import com.angelikafonteles.cursomc.repositories.CategoriaRepository;
 import com.angelikafonteles.cursomc.repositories.ProdutoRepository;
 import com.angelikafonteles.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -15,10 +21,19 @@ public class ProdutoService {
 	@Autowired
 	private ProdutoRepository repo;
 	
+	@Autowired
+	private CategoriaRepository CategoriaRepo;
+	
 	public Produto find(Integer id) {
 		
 		Optional<Produto> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
 	}
+	
+	 public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction){
+			PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+			List<Categoria> categorias = CategoriaRepo.findAllById(ids);
+			return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+	 }
 }
